7ede51cbd93f8b4b44647cfded8547a805ce0e3a
Dilani.Hewage <hdakanchana@gmail.com>
2024-08-29 14:15:06 +0530

    new election assignment

---
+++ src/main/java/com/example/New_election_tracking_assignment/NewElectionTrackingAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.New_election_tracking_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewElectionTrackingAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewElectionTrackingAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/New_election_tracking_assignment/NewElectionTrackingAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.New_election_tracking_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewElectionTrackingAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
